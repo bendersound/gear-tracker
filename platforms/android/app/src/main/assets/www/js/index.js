a3273bce97de62c75782d466bef968c256c60b2f
Ben Nichols <ben.nichols.dj@gmail.com>
2019-10-03 14:05:37 -0400

    partially implemented json stuff

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -8,7 +8,6 @@ var app = {
     document.getElementById("clearStorageButton").addEventListener("click", clearLocalStorage);
     document.getElementById("cameraButton").addEventListener("click", openCamera);
     document.getElementById("galleryButton").addEventListener("click", openGallery);
-    document.getElementById("clearImagebutton").addEventListener("click", hideImage);
 
     //Initializes disk storage object
     var localStorage = window.localStorage;
@@ -51,16 +50,30 @@ function popupDialog(title, message)
 function submitForm()
 {
   //Store form text
-  var caseInfoText = document.getElementById("caseInfoInput").value;
+  var caseEntry = {
+    make: document.getElementById("popupMake").value,
+    model: document.getElementById("popupModel").value,
+    amount: document.getElementById("popupAmount").value,
+    date: document.getElementById("popupDate").value,
+    other: document.getElementById("caseInfoInput").value,
 
+    toString : function()
+    {
+      return this.amount.toString() + "x " + this.make + " " + this.model;
+    }
+  };
+
+  var newCase = {id: caseIDText, entries: [caseEntry]};
+  var jsonData = JSON.stringify(newCase);
   //Temporarily use the caseID as a key for the case info
-
-  if (caseInfoText != "")
+  //popupDialog('', caseEntry.toString());
+  if (jsonData != "")
   {
-    localStorage.setItem(caseIDText, caseInfoText);
+    localStorage.setItem(caseIDText, jsonData);
     popupDialog('', "Successfully updated info");
   }
 }
+
 
 function openForm()
 {
@@ -69,11 +82,16 @@ function openForm()
   if (caseIDText != "")
   {
     document.getElementById("caseFormName").innerHTML = caseIDText;
-    var storedCaseInfo = localStorage.getItem(caseIDText);
+    var jsonData = localStorage.getItem(caseIDText);
 
-    if (storedCaseInfo != null)
+    if (jsonData != "")
     {
-      document.getElementById("caseInfoInput").value = storedCaseInfo;
+      //var case = $.parseJSON('{ "name":"John", "age":30, "city":"New York"}');
+      //popupDialog("", case.name);
+      //var case = JSON.parse(jsonData);
+      //var entry = case.entries[0];
+      //document.getElementById("popupMake").value = entry.make;
+      document.getElementById("caseInfoInput").value = jsonData;
     }
 
     document.getElementById("caseFormPopup").style.display = "block";
@@ -127,13 +145,6 @@ function openGallery()
   {
     popupDialog('Error!', message);
   }
-}
-
-//todo: does not keep img border
-function hideImage()
-{
-  qr_image = document.getElementById("qr_image_frame");
-  qr_image.style.visibility = 'hidden';
 }
 
 app.initialize();
