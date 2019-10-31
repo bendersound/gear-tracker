b7cc22bf23443c48295ce7c820b958c2c431e7c1
Ben Nichols <ben.nichols.dj@gmail.com>
2019-10-31 15:14:06 -0400

    Rendering table of JSON objects properly. Updating disk protocol.

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -3,58 +3,52 @@ var app = {
   initialize: function() {
     //initialize listeners
     document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    document.addEventListener('pause', this.onPause.bind(this), false);
-    document.addEventListener('resume', this.onResume.bind(this), false);
+    document.addEventListener(' pause', this.onPause.bind(this), false);
     document.getElementById('submitCaseButton').addEventListener('click', openCaseFormManual);
     document.getElementById('submitFormButton').addEventListener('click', submitForm);
     document.getElementById('clearStorageButton').addEventListener('click', clearLocalStorage);
     document.getElementById('cameraButton').addEventListener('click', scanQR);
 
     //Initializes disk storage object
-    var localStorage;
+    var localStorage = window.localStorage;
+    var numberOfCases;
+    var caseList;
     var caseIDText;
-
-    var caseEntries;
   },
 
-    // Handle device ready
-  onDeviceReady: function() {
-      //this.receivedEvent('deviceready');
+  onDeviceReady: function()
+  {
+      storeTestCases();
+
       openCamera();
-      localStorage = window.localStorage;
-      caseEntries = localStorage.getItem('caseEntries').valueOf();
-      if(!caseEntries)
-      {
-        caseEntries = 0;
-      }
+      caseList = openCases();
+      var listParent = document.getElementById('caseList');
+      renderCaseList(listParent);
+      popupDialog('Test', 'Success');
   },
 
-  // Handle pause
   onPause: function()
   {
-    //keep track of number of cases we are working with
-    localStorage.setItem('caseEntries', caseEntries.toString());
-  },
-
-  // Handle resume
-  onResume: function()
-  {
+    storeCases();
   }
-
-  // Update DOM on a Received Event
-  /*
-  , receivedEvent: function(id) {
-    var parentElement = document.getElementById(id);
-    var listeningElement = parentElement.querySelector('.listening');
-    var receivedElement = parentElement.querySelector('.received');
-
-    listeningElement.setAttribute('style', 'display:none;');
-    receivedElement.setAttribute('style', 'display:block;');
-
-    console.log('Received Event: ' + id);
-  }
-  */
 };
+
+/*
+// TODO: Testing
+*/
+function storeTestCases()
+{
+  var testCase1 = {id:'Test 1', equipment_count:1};
+  var testCase2 = {id:'Test 2', equipment_count:2};
+  var testCase3 = {id:'Test 3', equipment_count:3};
+
+  caseList = [testCase1, testCase2, testCase3];
+  numberOfCases = 3;
+
+  clearLocalStorage();
+  storeCases();
+  clearGlobalParamters();
+}
 
 // Popup dialog for testing
 function popupDialog(title, message)
@@ -76,45 +70,27 @@ function submitForm()
   const caseInfoText = document.getElementById('caseInfoInput').value;
   //Temporarily use the caseID as a key for the case info
 
+  popupDialog('Test', this.numberOfCases.toString());
   if (caseInfoText != '')
   {
     localStorage.setItem(caseIDText, caseInfoText);
     popupDialog('', 'Successfully updated info');
-    popupDialog('Number of Case Entries', caseEntries);
   }
-}
-
-function openGearEntry()
-{
-  var gearEntry = {
-    make: '',
-    model: '',
-    amount: '',
-    datePurchased: '',
-    otherInfo: ''
-  };
 }
 
 //open form for viewing
 //param caseIDText used for passing in case ID
 function openCaseForm(caseID)
 {
-  var caseEntry = {
-    name: '',
-    gearEntries: [],
-    storageLocation: '',
-    dateLastUsed: '',
-    otherInfo: ''
-  };
-  //if (caseID)
+  //if (caseID != '')
   //{
+    document.getElementById('caseFormPopup').style.display = 'block';
+    document.getElementById('caseFormName').innerHTML = caseID;
     const storedCaseInfo = localStorage.getItem(caseID);
     if (storedCaseInfo)
     {
       document.getElementById('caseInfoInput').value = storedCaseInfo;
     }
-    document.getElementById('caseFormName').innerHTML = caseID;
-    document.getElementById('caseFormPopup').style.display = 'block';
     caseIDText = caseID;
   //}
 }
@@ -126,10 +102,19 @@ function openCaseFormManual()
   openCaseForm(caseIDText);
 }
 
+//
+clearGlobalParamters()
+{
+  numberOfCases = 0;
+  cases = [];
+  caseIDText = '';
+}
+
 //clear data on disk
 function clearLocalStorage()
 {
   localStorage.clear();
+  clearGlobalParamters();
   popupDialog('', 'Local storage cleared');
 }
 
@@ -181,6 +166,64 @@ function openCamera()
   {
     console.log(status);
   });
+}
+
+function renderCaseList(caseListParent)
+{
+  var newCaseListing;
+  var tableRow;
+  var idCell;
+  var equipCell;
+
+  for (i = 0; i < caseList.length; i++)
+  {
+    //cases.push(localStorage.getItem(i.toString()));
+    tableRow = document.createElement("tr");
+
+    idCell = document.createElement("td");
+    equipCell = document.createElement("td");
+
+    idCell.innerHTML = caseList[i].id;
+    equipCell.innerHTML = caseList[i].equipment_count;
+
+    tableRow.appendChild(idCell);
+    tableRow.appendChild(equipCell);
+    caseListParent.appendChild(tableRow);
+  }
+  //// TODO: for testing only
+  document.getElementById('leftPanel').style.display = 'block';
+}
+
+function openCases()
+{
+  numberOfCases = localStorage.getItem('numberOfCases');
+  if (!numberOfCases)
+  {
+    numberOfCases = 0;
+  }
+
+  var cases = [];
+  var caseListingStr = '';
+  var caseListingJSON = {};
+
+  for (i = 0; i < numberOfCases; i++)
+  {
+    caseListingStr = localStorage.getItem('case' + i.toString());
+    caseListingJSON = JSON.parse(caseListingStr);
+    cases.push(caseListing);
+  }
+
+  return cases;
+}
+
+function storeCases()
+{
+  localStorage.setItem('number_of_cases', numCases.toString());
+
+  for (i = 0; i < caseList.length; i++)
+  {
+    localStorage.setItem('case' + i.toString(), caseList[i].toString());
+  }
 }
 
 app.initialize();
--- www/js/index.js
+++ www/js/index.js
@@ -3,6 +3,7 @@ var app = {
   initialize: function() {
     //initialize listeners
     document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    document.addEventListener(' pause', this.onPause.bind(this), false);
     document.getElementById('submitCaseButton').addEventListener('click', openCaseFormManual);
     document.getElementById('submitFormButton').addEventListener('click', submitForm);
     document.getElementById('clearStorageButton').addEventListener('click', clearLocalStorage);
@@ -10,34 +11,44 @@ var app = {
 
     //Initializes disk storage object
     var localStorage = window.localStorage;
-    var numberOfCases = 0;
+    var numberOfCases;
+    var caseList;
     var caseIDText;
   },
 
-  onDeviceReady: function() {
-      //this.receivedEvent('deviceready');
+  onDeviceReady: function()
+  {
+      storeTestCases();
+
       openCamera();
-      tmpNumCases = localStorage.getItem('numberOfCases');
-      if (tmpNumCases)
-      {
-        numberOfCases = tmpNumCases
-      }
+      caseList = openCases();
+      var listParent = document.getElementById('caseList');
+      renderCaseList(listParent);
+      popupDialog('Test', 'Success');
   },
-  
-  // Update DOM on a Received Event
-  /*
-  receivedEvent: function(id) {
-    var parentElement = document.getElementById(id);
-    var listeningElement = parentElement.querySelector('.listening');
-    var receivedElement = parentElement.querySelector('.received');
 
-    listeningElement.setAttribute('style', 'display:none;');
-    receivedElement.setAttribute('style', 'display:block;');
-
-    console.log('Received Event: ' + id);
+  onPause: function()
+  {
+    storeCases();
   }
-  */
 };
+
+/*
+// TODO: Testing
+*/
+function storeTestCases()
+{
+  var testCase1 = {id:'Test 1', equipment_count:1};
+  var testCase2 = {id:'Test 2', equipment_count:2};
+  var testCase3 = {id:'Test 3', equipment_count:3};
+
+  caseList = [testCase1, testCase2, testCase3];
+  numberOfCases = 3;
+
+  clearLocalStorage();
+  storeCases();
+  clearGlobalParamters();
+}
 
 // Popup dialog for testing
 function popupDialog(title, message)
@@ -59,6 +70,7 @@ function submitForm()
   const caseInfoText = document.getElementById('caseInfoInput').value;
   //Temporarily use the caseID as a key for the case info
 
+  popupDialog('Test', this.numberOfCases.toString());
   if (caseInfoText != '')
   {
     localStorage.setItem(caseIDText, caseInfoText);
@@ -90,10 +102,19 @@ function openCaseFormManual()
   openCaseForm(caseIDText);
 }
 
+//
+clearGlobalParamters()
+{
+  numberOfCases = 0;
+  cases = [];
+  caseIDText = '';
+}
+
 //clear data on disk
 function clearLocalStorage()
 {
   localStorage.clear();
+  clearGlobalParamters();
   popupDialog('', 'Local storage cleared');
 }
 
@@ -145,6 +166,64 @@ function openCamera()
   {
     console.log(status);
   });
+}
+
+function renderCaseList(caseListParent)
+{
+  var newCaseListing;
+  var tableRow;
+  var idCell;
+  var equipCell;
+
+  for (i = 0; i < caseList.length; i++)
+  {
+    //cases.push(localStorage.getItem(i.toString()));
+    tableRow = document.createElement("tr");
+
+    idCell = document.createElement("td");
+    equipCell = document.createElement("td");
+
+    idCell.innerHTML = caseList[i].id;
+    equipCell.innerHTML = caseList[i].equipment_count;
+
+    tableRow.appendChild(idCell);
+    tableRow.appendChild(equipCell);
+    caseListParent.appendChild(tableRow);
+  }
+  //// TODO: for testing only
+  document.getElementById('leftPanel').style.display = 'block';
+}
+
+function openCases()
+{
+  numberOfCases = localStorage.getItem('numberOfCases');
+  if (!numberOfCases)
+  {
+    numberOfCases = 0;
+  }
+
+  var cases = [];
+  var caseListingStr = '';
+  var caseListingJSON = {};
+
+  for (i = 0; i < numberOfCases; i++)
+  {
+    caseListingStr = localStorage.getItem('case' + i.toString());
+    caseListingJSON = JSON.parse(caseListingStr);
+    cases.push(caseListing);
+  }
+
+  return cases;
+}
+
+function storeCases()
+{
+  localStorage.setItem('number_of_cases', numCases.toString());
+
+  for (i = 0; i < caseList.length; i++)
+  {
+    localStorage.setItem('case' + i.toString(), caseList[i].toString());
+  }
 }
 
 app.initialize();
